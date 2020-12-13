85f80f3812b243cee5e444b2364bc7f4be573e87
Ilya <iljuevgrafov@yandex.ru>
2020-12-13 11:59:29 +0300

    mail yml changed

--- hello.py
+++ hello.py
@@ -6,6 +6,5 @@ app = Flask(__name__)
 def hello_world():
     return 'У меня получилось!'
 
-'sdfsdfwefwvmdfnd ssopdruoghj;wegrhwqer98gvh8934v9vuwerv9wervhqiurhvuhwsar9hv8uqwervuophwer98vhshfruvhsdufphv9-8werhvupwerl;vhwe-89v-9uvh3u4vhq23-h4f3'
 if __name__ == '__main__':
     app.run(debug=True, host='0.0.0.0')
